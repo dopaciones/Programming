f4fcfe4e50f0ca5e51e8897428c8c6bd0af19b2e
dopaciones <56845176+dopaciones@users.noreply.github.com>
2019-11-29 17:44:37 +0300

    Text (done)

--- Text/main.cpp
+++ Text/main.cpp
@@ -1,3 +1,5 @@
+#include "pch.h"
+#include "string"
 #include <iostream>
 #include <fstream>
 #include <windows.h>
@@ -6,33 +8,40 @@ using namespace std;
 
 int main()
 {
-    int i = 0;
-    int count = 0;
-    string s = "";
+	int i = 0;
+	int min = INT_MAX;
+	int count = 0;
+	int str_len = 0;
+	int phr_len = 0;
+	string s = "";
 
-    setlocale(LC_ALL, "RUS");
+	setlocale(LC_ALL, "RUS");
 
-    ofstream fin;
-    fin.open("Test1.txt");
-    while (s != "="){
-        getline(cin,s);
-        fin << s << endl;
-    }
-    fin.close();
+	ifstream fout;
+	fout.open("Test.txt");
+	while (!fout.eof()) {
+		getline(fout, s);
+		str_len = s.length();
+		while (i < str_len) {
+			if ((s[i] == '!') || (s[i] == '?') || (s[i] == '.') || (s[i] == ';')) {
+				phr_len = count;
+				if (phr_len < min) {
+					min = phr_len;
+				}
+				count = 0;
+				i++;
+			}
+			else {
+				count++;
+				i++;
+			}
+		}
+		cout << s << endl;
+		i = 0;
+	}
+	fout.close();
 
-    ifstream fout;
-    fout.open("Test1.txt");
-    while (!fout.eof()){
-        getline(fout,s);
-        while ((s[i] != '.')||(s[i] != '?')||(s[i] != '!')||(s[i] != ';')){
-            count ++;
-        }
-        if ((s[i] != '.')||(s[i] != '?')||(s[i] != '!')||(s[i] != ';')){
+	cout << endl;
 
-        }
-        cout << s << endl;
-    }
-    fout.close();
-
-    cout << count;
+	cout << "Minimal phrase length - "<< min;
 }
